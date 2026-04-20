2cd174eddad6ff3c443852faae8a78c147dc79e2
GermanGVX <vallejosgerman2004@gmail.com>
2026-04-20 02:12:47 -0300

    Configurar DbContext con nombres exactos de tablas y seed data, todas las carpetas de caso de uso, mas ejemplo de controller API

---
+++ TP1/Trabajo_Practico_Proyecto_de_Software/Controllers/EventsController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trabajo_Practoco_Proyecto_de_Software.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly IServicesGetAll _services;
+
+        public EventsController(IServicesGetAll services)
+        {
+            _services = services;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result= _services.GetAll();
+            return new JsonResult(result);
+        }
+    }
+}
--- TP1/Trabajo_Practico_Proyecto_de_Software/Program.cs
+++ TP1/Trabajo_Practico_Proyecto_de_Software/Program.cs
@@ -1,3 +1,8 @@
+using Application.Interfaces;
+using Application.UseCases.Events.Querys;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// TP
+var connectionString = builder.Configuration["connectionString"];
+builder.Services.AddDbContext<AppDbContext>(option => option.UseSqlServer(connectionString));
+
+
+builder.Services.AddTransient<IServicesGetAll, GetAllEventsQuery>();
+
+
+
+
+
 
 var app = builder.Build();
 
